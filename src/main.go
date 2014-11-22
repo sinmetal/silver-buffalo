74a221c532023330e0bc0e1303aa188f27b398aa
Shingo Ishimura <metal.tie@gmail.com>
2014-11-22 12:28:59 +0900

    add bigquery streaming api sample

--- src/main.go
+++ src/main.go
@@ -25,16 +25,54 @@ func main() {
 		fmt.Errorf("%v", err)
 	}
 
+	insert(bq)
+	list(bq)
+}
+
+func list(bq *bigquery.Service) {
 	call := bq.Tabledata.List("sinpkmnms", "sample", "persons")
 	call.MaxResults(10)
 	list, err := call.Do()
 	if err != nil {
 		fmt.Errorf("%v", err)
+		return
 	}
 
 	buf, err := json.Marshal(list)
 	if err != nil {
 		fmt.Errorf("%v", err)
+		return
 	}
 	fmt.Println(string(buf))
+}
+
+func insert(bq *bigquery.Service) {
+	rows := make([]*bigquery.TableDataInsertAllRequestRows, 2)
+	rows[0] = &bigquery.TableDataInsertAllRequestRows{
+		Json: map[string]bigquery.JsonValue{
+			"id":    "ida",
+			"item1": "item1a",
+			"item2": "item2a",
+			"item3": "item3a",
+			"date":  "2014/01/01",
+		},
+	}
+	rows[1] = &bigquery.TableDataInsertAllRequestRows{
+		Json: map[string]bigquery.JsonValue{
+			"id":    "idb",
+			"item1": "item1b",
+			"item2": "item2b",
+			"item3": "item3b",
+			"date":  "2014/02/02",
+		},
+	}
+
+	_, err := bq.Tabledata.InsertAll("sinpkmnms", "sample", "test", &bigquery.TableDataInsertAllRequest{
+		Kind: "bigquery#tableDataInsertAllRequest",
+		Rows: rows,
+	}).Do()
+	if err != nil {
+		fmt.Errorf("%v", err)
+		return
+	}
 }
