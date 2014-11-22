7ba2063dca45d465b36188495c6344bd289c4239
Shingo Ishimura <metal.tie@gmail.com>
2014-11-22 18:20:56 +0900

    add fetch target app

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -27,18 +28,19 @@ func main() {
 		fmt.Errorf("%v", err)
 	}
 
-	url := "http://1.cp300demo1.appspot.com/"
-	startNano := time.Now().UnixNano()
-	resp, err := http.Get(url)
-	endNano := time.Now().UnixNano()
+	const url = "http://1.cp300demo1.appspot.com/"
+	var i int = 0
+	var wg sync.WaitGroup
+	for {
+		for j := 0; j < 50; j++ {
+			wg.Add(1)
+			go run(fmt.Sprint(i, ":", j), url, bq, &wg)
+		}
+		i++
+		time.Sleep(1 * time.Second)
+	}
+	wg.Wait()
 
-	const ns = 1000000000
-	const mics = 1000000
-
-	start := time.Unix(startNano/ns, startNano%ns)
-	end := time.Unix(endNano/ns, endNano%ns)
-
-	insert(bq, url, resp.StatusCode, start.Unix(), end.Unix(), (endNano-startNano)/mics)
 	//list(bq)
 }
 
@@ -59,7 +61,34 @@ func list(bq *bigquery.Service) {
 	fmt.Println(string(buf))
 }
 
-func insert(bq *bigquery.Service, url string, statusCode int, start int64, end int64, ns int64) {
+func run(id string, url string, bq *bigquery.Service, wg *sync.WaitGroup) {
+	fmt.Println("start : ", id)
+
+	startNano := time.Now().UnixNano()
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		wg.Done()
+		return
+	}
+	endNano := time.Now().UnixNano()
+
+	const ns = 1000000000
+	const mics = 1000000
+
+	start := time.Unix(startNano/ns, startNano%ns)
+	end := time.Unix(endNano/ns, endNano%ns)
+
+	err = insert(bq, url, resp.StatusCode, start.Unix(), end.Unix(), (endNano-startNano)/mics)
+	if err != nil {
+		fmt.Println("ng : ", id, err.Error())
+	} else {
+		fmt.Println("done : ", id)
+	}
+	wg.Done()
+}
+
+func insert(bq *bigquery.Service, url string, statusCode int, start int64, end int64, ms int64) error {
 	rows := make([]*bigquery.TableDataInsertAllRequestRows, 1)
 	rows[0] = &bigquery.TableDataInsertAllRequestRows{
 		Json: map[string]bigquery.JsonValue{
@@ -67,16 +96,22 @@ func insert(bq *bigquery.Service, url string, statusCode int, start int64, end i
 			"status_code": statusCode,
 			"start":       start,
 			"end":         end,
-			"progres_ms":  ns,
+			"progres_ms":  ms,
 		},
 	}
 
-	_, err := bq.Tabledata.InsertAll("sinpkmnms", "dos", "progres", &bigquery.TableDataInsertAllRequest{
-		Kind: "bigquery#tableDataInsertAllRequest",
-		Rows: rows,
-	}).Do()
-	if err != nil {
-		fmt.Errorf("%v", err)
-		return
+	var err error
+	for i := 1; i < 10; i++ {
+		_, err = bq.Tabledata.InsertAll("sinpkmnms", "dos", "progres20141122", &bigquery.TableDataInsertAllRequest{
+			Kind: "bigquery#tableDataInsertAllRequest",
+			Rows: rows,
+		}).Do()
+		if err != nil {
+			fmt.Errorf("%v", err)
+			time.Sleep(time.Duration(i) * time.Second)
+		} else {
+			break
+		}
 	}
+	return err
 }
